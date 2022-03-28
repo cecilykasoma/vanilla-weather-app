a4a2f14322588231f01d89646d6e62cfc6bcd399
cecilykasoma <99963992+cecilykasoma@users.noreply.github.com>
2022-03-28 10:32:05 +0100

    showing current weather details

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
 function displayTemperature(response) {
-  console.log(response.data.weather[0].description);
+  console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
+  let precipitationElement = document.querySelector("#precipitation");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
+  //   precipitationElement.innerHTML = response.data.main.precipitation;
+  humidityElement.innerHTML = Math.round(response.data.main.humidity);
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
 let apiKey = "f40ed1916a487f9b9d24602c83f9b430";
