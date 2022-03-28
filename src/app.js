9e14c3bf35252c858420ec416fe0125d7fc9b5b0
cecilykasoma <99963992+cecilykasoma@users.noreply.github.com>
2022-03-28 10:10:44 +0100

    API's for city, temperature and description

--- src/app.js
+++ src/app.js
@@ -1,1 +1,15 @@
+function displayTemperature(response) {
+  console.log(response.data.weather[0].description);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+}
 
+let apiKey = "f40ed1916a487f9b9d24602c83f9b430";
+let city = "New York";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
