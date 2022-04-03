c87f602449f246391c8557827d8231b9e8607d3f
cecilykasoma <99963992+cecilykasoma@users.noreply.github.com>
2022-04-03 09:20:18 +0100

    create HTML from js template

--- src/app.js
+++ src/app.js
@@ -78,7 +78,26 @@ function displayCelciusTemperature(event) {
 // weather forecast
 function displayForecast() {
   let forecastElement = document.querySelector("#forecast");
-  forecastElement.innerHTML = "Forecast";
+  forecastHTML = `<div class="row">`;
+  let days = ["Sat", "Sun", "Mon", "Tue", "Wed", "Thu"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `<div class="col-2">
+              <div class="weather-forecast-date">${day}</div>
+              <img
+                src="http://openweathermap.org/img/wn/03d@2x.png"
+                alt=""
+                width="40px"
+              />
+              <div class="weather-forecast-temperatures">
+                <span class="weather-forecast-temperature-max">8° </span>
+                <span class="weather-forecast-temperature-min"> 2°</span>
+              </div>
+            </div>`;
+  });
+  forecastHTML = forecastHTML + `</div>;`;
+  forecastElement.innerHTML = forecastHTML;
 }
 
 let celciusTemperature = null;
